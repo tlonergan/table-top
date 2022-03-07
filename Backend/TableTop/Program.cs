6abf0e1b1b3a6355561ed8c23df9d0541ae5ec8f
Travis Lonergan <travislonergan@gmail.com>
2022-03-07 16:13:45 -0700

    Troubleshooting

--- Backend/TableTop/Program.cs
+++ Backend/TableTop/Program.cs
@@ -39,10 +39,12 @@ app.UseAuthorization();
 
 PhysicalFileProvider physicalFileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "App"));
 var contents = physicalFileProvider.GetDirectoryContents(string.Empty);
+Console.WriteLine("Files in './App'");
 foreach (IFileInfo content in contents)
 {
     Console.WriteLine(content.Name);
 }
+
 StaticFileOptions staticFileOptions = new StaticFileOptions { FileProvider = physicalFileProvider, RequestPath = "" };
 app.UseStaticFiles(staticFileOptions);
 
