fec4e14c4e2ab5ec668da7be859ffdcfe23ad8e4
Travis Lonergan <travislonergan@gmail.com>
2022-03-23 13:38:12 -0700

    Edit mode creation

--- Frontend/src/components/boardSummary.jsx
+++ Frontend/src/components/boardSummary.jsx
@@ -2,21 +2,20 @@ import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 import { useAtom } from "jotai";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
 
 import { updateGameBoardAtom } from "../state/game";
 import { updateBoard } from "../services/boardService";
 import Card from './card';
 
 const BoardSummary = ({state, gameId}) => {
-    console.log("BoardSummary => Render", state, gameId);
-
     const navigate = useNavigate();
     const { getAccessTokenSilently } = useAuth0();
 
     const [board, setBoard] = useAtom(state);
     const [cardButtons, setCardButtons] = useState([]);
-
-    const [, updateStateBoard] = useAtom(updateGameBoardAtom);
+    const [isEditMode, setIsEditMode] = useState(false);
 
     useEffect(() => {
         let buttons = [{display: 'Go To Board', onClick: goToBoardClicked}];
@@ -40,15 +39,50 @@ const BoardSummary = ({state, gameId}) => {
         });
     };
 
+    const editClicked = () => {
+        setIsEditMode(true);
+    };
+
+    const saveClicked = () => {
+        setIsEditMode(false);
+    };
+
+    if(isEditMode){
+        return (
+            <Card name={`${board.name} ${board.isActive ? '(Active)' : ''} - Editing`} buttons={[{display: "Save", onClick: saveClicked}]}>
+                <div className="form">
+                    <div>
+                        <label>Name</label>
+                        <input type="text" value={board.name} />
+                    </div>
+                    <div>
+                        <label>Width </label>
+                        <input type="text" value={board.width} />
+                    </div>
+                    <div>
+                        <label>Height </label>
+                        <input type="text" value={board.height} />
+                    </div>
+                </div>
+            </Card>);
+    }
+
     return (
-        <Card name={board.isActive ? `${board.name} (Active)`: board.name} buttons={cardButtons}>
-            <div>
-                <label>Width: </label>
-                <span>{board.width}</span>
-            </div>
-            <div>
-                <label>Height: </label>
-                <span>{board.height}</span>
+        <Card name={`${board.name} ${board.isActive ? '(Active)' : ''}`} buttons={cardButtons}>
+            <div style={{display: "flex", justifyContent: "space-between", flexDirection: "row-reverse"}}>
+                <div>
+                    <a onClick={editClicked}><FontAwesomeIcon icon={faPencil} /></a>
+                </div>
+                <div>
+                    <div>
+                        <label>Width: </label>
+                        <span>{board.width}</span>
+                    </div>
+                    <div>
+                        <label>Height: </label>
+                        <span>{board.height}</span>
+                    </div>
+                </div>
             </div>
         </Card>
     );
