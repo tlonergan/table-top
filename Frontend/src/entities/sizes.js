71379d343b00df4d23ae0d684ac3c30b7a1e5c13
Travis Lonergan <travislonergan@gmail.com>
2022-03-24 09:53:56 -0700

    Putting size definition in

---
+++ Frontend/src/entities/sizes.js
@@ -0,0 +1,34 @@
+const Sizes = {
+    GARGANTUAN: {
+        name: 'Gargantuan',
+        height: 20,
+        width: 20,
+    },
+    HUGE: {
+        name: 'Huge',
+        height: 15,
+        width: 15,
+    },
+    LARGE: {
+        name: 'Large',
+        height: 10,
+        width: 10,
+    },
+    MEDIUM: {
+        name: 'Meduim',
+        height: 5,
+        width: 5,
+    },
+    SMALL: {
+        name: 'Small',
+        height: 5,
+        width: 5,
+    },
+    TINY: {
+        name: 'Tiny',
+        height: 2.5,
+        width: 2.5,
+    }
+};
+
+export default Sizes;
